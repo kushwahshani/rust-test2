c44da509e1b394808d529d95abae613a92e8afe8
kushwahshani <sanikushwah3190@gmail.com>
2025-01-06 17:06:42 +0530

    change query

--- src/routes/auth.rs
+++ src/routes/auth.rs
@@ -1,6 +1,7 @@
 // use crate::wasm_bindgen::JsValue;
 // use reqwest::Client;
 use serde::{Deserialize, Serialize};
+use serde_json::json;
 use worker::{console_log, Request, Response, Result, RouteContext, Url};
 
 // use super::shop_name;
@@ -13,44 +14,66 @@ struct Shop {
 }
 
 pub async fn auth(req: Request, ctx: RouteContext<()>) -> Result<Response> {
+    // console_log!("this is auth function");
     // let client_id = "fe56c0cf0d804e83ddbbce365e1c2353";
     // let scope = "read_products";
     // let redirect_url = "https://test2.shanikushwahonline.workers.dev/token";
     // Get D1 database binding
+    let url = req.url()?;
     let d1 = ctx.env.d1("DB")?;
 
-    // Extract the shop ID from the query parameters
-    let url = req.url()?;
+    // // Extract the shop ID from the query parameters
     let shop_name = url
         .query_pairs()
         .find(|(key, _)| key == "shop")
         .map(|(_, value)| value);
 
+    
     // if shop_name.is_empty() {
-    //     return Response::error("Shop name is required", 400);
+        //     return Response::error("Shop name is required", 400);
+        // }
+        
+        if let Some(shop_name) = shop_name {
+            let check_query = "SELECT shop, auth_token, installation FROM shops WHERE shop = ?";
+                let statement = d1
+                    .prepare(check_query)
+                    .bind(&["ac-dev-25.myshopify.com".to_string().into()])?;
+                let query_result = statement.first::<Shop>(None).await?;
+                    //  let results: Option<Shop> = statement.first(None).await?;
+                     if let Some(value) = query_result {
+                        return Response::from_json(&json!({
+                            "status": "success",
+                            "message": "this is a result",
+                            "auth_token": shop_name
+                        }));
+                     }else {
+                         return Response::ok("this is query result:");
+                     }
+            return Response::from_json(&json!({
+                "status": "success",
+                "message": "this is shop name",
+                "auth_token": shop_name
+            }));
+        }
+        return Response::from_json(&json!({
+            "status": "success",
+            "message": "Token generated and shop installed",
+            "auth_token": shop_name
+        }));
+    //     console_log!("test 2{}", shop_name.clone());
+
+
+    //     // Check if the shop is already installed
+    //     // let check_query = "SELECT shop, auth_token, installation FROM shops WHERE shop = ?";
+
+   
+
+    //     console_log!("Query result: {:?}", results);
+    //     return Response::ok("shop name");
     // }
-    // Response::ok(format!("Received shop name: {}", shop_name))?;
-
-    if let Some(shop_name) = shop_name {
-        console_log!("test 2{}", shop_name.clone());
-
-        let check_query = "SELECT shop, auth_token, installation FROM appshop WHERE shop = ?";
-
-        // Check if the shop is already installed
-        // let check_query = "SELECT shop, auth_token, installation FROM shops WHERE shop = ?";
-
-        let statement = d1
-            .prepare(check_query)
-            .bind(&["ac-dev-25.myshopify.com".to_string().into()])?;
-        // let query_result = statement.first::<Shop>(None).await?;
-        let results: Option<Shop> = statement.first(None).await?;
-
-        console_log!("Query result: {:?}", results);
-        return Response::ok("shop name");
-    }
-    else {
-        return Response::error("this is an error", 400);
-    }
+    // else {
+    //     return Response::error("this is an error", 400);
+    // }
     // if let Some(shop) = query_result {
     //     if shop.installation == 1.0 {
     //         let redirect_url = Url::parse("https://shopify-test1.pages.dev/home")?;
@@ -67,7 +90,7 @@ pub async fn auth(req: Request, ctx: RouteContext<()>) -> Result<Response> {
     // }
 
     // Return a response including the received shop name
-    // let response = Response::ok(format!("Received shop name: {}", shop_name))?;
+    let response = Response::ok(format!("Received shop name:"))?;
 
-    // Ok(response)
+    Ok(response)
 }
