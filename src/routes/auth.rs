76cea802ac859396bbbd69ce7edeee6c2e3814b2
kushwahshani <sanikushwah3190@gmail.com>
2025-01-06 13:18:40 +0530

    comment the code

--- src/routes/auth.rs
+++ src/routes/auth.rs
@@ -24,36 +24,36 @@ pub async fn auth(req: Request, ctx: RouteContext<()>) -> Result<Response> {
         .find(|(key, _)| key == "shop")
         .map(|(_, value)| value.to_string())
         .unwrap_or_default(); // Default to an empty string if "shop" is not found
-    console_log!("Shop name extracted: {}", shop_name);
+    // console_log!("Shop name extracted: {}", shop_name);
 
-    if shop_name.is_empty() {
-        return Response::error("Shop name is required", 400);
-    }
+    // if shop_name.is_empty() {
+    //     return Response::error("Shop name is required", 400);
+    // }
 
-    // Check if the shop is already installed
-    let check_query = "SELECT shop, auth_token, installation FROM shops WHERE shop = ?";
-    let statement = d1
-        .prepare(check_query)
-        .bind(&[JsValue::from(shop_name.clone())])?;
-    let query_result = statement.first::<Shop>(None).await?;
+    // // Check if the shop is already installed
+    // let check_query = "SELECT shop, auth_token, installation FROM shops WHERE shop = ?";
+    // let statement = d1
+    //     .prepare(check_query)
+    //     .bind(&[JsValue::from(shop_name.clone())])?;
+    // let query_result = statement.first::<Shop>(None).await?;
 
-    if let Some(shop) = query_result {
-        if shop.installation == 1.0 {
-            let redirect_url = Url::parse("https://shopify-test1.pages.dev/home")?;
-            return Response::redirect(redirect_url);
-        } else {
-            return Response::error("Shop exists but is not installed", 400);
-        }
-    } else {
-        let shop_url = Url::parse(&format!(
-                        "https://{}/admin/oauth/authorize?client_id={}&scope={}&redirect_uri={}&state=nonce",
-                        shop_name, client_id, scope, redirect_url
-                    ))?;
-        return Response::redirect(shop_url);
-    }
+    // if let Some(shop) = query_result {
+    //     if shop.installation == 1.0 {
+    //         let redirect_url = Url::parse("https://shopify-test1.pages.dev/home")?;
+    //         return Response::redirect(redirect_url);
+    //     } else {
+    //         return Response::error("Shop exists but is not installed", 400);
+    //     }
+    // } else {
+    //     let shop_url = Url::parse(&format!(
+    //                     "https://{}/admin/oauth/authorize?client_id={}&scope={}&redirect_uri={}&state=nonce",
+    //                     shop_name, client_id, scope, redirect_url
+    //                 ))?;
+    //     return Response::redirect(shop_url);
+    // }
 
     // Return a response including the received shop name
-    // let response = Response::ok(format!("Received shop name: {}", shop_name))?;
+    let response = Response::ok(format!("Received shop name: {}", shop_name))?;
 
-    // Ok(response)
+    Ok(response)
 }
